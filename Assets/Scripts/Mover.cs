41b836e90f0f900b5fcf9f838da9cf8d401e7773
Luis Borjas <luisfborjas@gmail.com>
2015-11-26 17:46:27 -0600

    Lesson 6: Creating shots
    Able to create a template prefab, add a material, tweak the shader
    (mobile shaders are less greedy with resources) and then have fun
    dragging prefabs into the hierarchy while on game mode to see them
    shoot across the screen.
    
    http://unity3d.com/learn/tutorials/projects/space-shooter/creating-shots?playlist=17147

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+	public float speed;
+
+	private Rigidbody rb;
+	// Use this for initialization: executed on the first frame
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = transform.forward * speed;
+	}
+}
