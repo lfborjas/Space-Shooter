ee89ea49101f75e1b9299fe54db0a74b88796321
Luis Borjas <luisfborjas@gmail.com>
2015-11-27 00:50:09 -0600

    Lesson 17b: adds particles, scrolling background
    Uses a V transform to copy the background and some scripting to make it
    scroll; plus some dope particle generators that emit stars to simulate a
    starfield.
    
    Minutes 14-33:34 of
    http://unity3d.com/learn/tutorials/modules/beginner/live-training-archive/extending-space-shooter?playlist=17147

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScroller : MonoBehaviour {
+
+	public float scrollSpeed;
+	public float tileSizeZ;
+
+	private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newPosition = Mathf.Repeat (Time.time * scrollSpeed, tileSizeZ);
+		// Vector3.forward == (0,0,1);
+		transform.position = startPosition + Vector3.forward * newPosition;
+	}
+}
