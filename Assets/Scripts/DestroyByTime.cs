21d5b7700ceb6e3bc1606c968e7ebcd38846c22a
Luis Borjas <luisfborjas@gmail.com>
2015-11-26 21:40:02 -0600

    Lesson 12: infinite waves of asteroids
    Also introduces a script to destroy the explosion objects a couple of
    seconds before they're instantiated, to not create too many objects that
    never die.
    
    http://unity3d.com/learn/tutorials/projects/space-shooter/spawning-waves?playlist=17147

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour {
+	public float lifetime;
+
+	void Start(){
+		// we can specify a wait time before destroy actually takes effect
+		Destroy (gameObject, lifetime);
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,14 +4,27 @@ using System.Collections;
 public class GameController : MonoBehaviour {
 	public GameObject hazard;
 	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
 
 	void Start(){
-		SpawnWaves ();
+		StartCoroutine (SpawnWaves ());
 	}
 
-	void SpawnWaves(){
-		Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate (hazard, spawnPosition, spawnRotation);
+	IEnumerator SpawnWaves(){
+		yield return new WaitForSeconds (startWait);
+		// keep creating waves until the player dies
+		while(true){
+			for (int i = 0; i < hazardCount; i++) {
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				//don't create them all at the same time, needs the function to be a coroutine to not block the loop
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+		}
 	}
 }
