f8c1b1b8c0df855919be7dd89cc8250a3f40b08f
Luis Borjas <luisfborjas@gmail.com>
2015-11-28 15:52:16 -0600

    Lesson 18a: unity remote, device input
    Using the magic of unity remote, the accelerometer and "Fire1"'s default
    binding to any touch event to stream the game physically on my iPhone!
    Interesting how we need to "calibrate" to the device if we want to use
    the accelerometer comfortably, rather than just use the default 3D zero
    point that expects the phone to be parallel to the ground.
    
    Made a bunch of tweaks to assets, too, so they'd render correctly on
    iPhone: using truecolor for sprites, compressing the background a bit.
    
    Had to change the build settings and also the player settings to build
    to iOS and use unity remote, respectively.
    
    First 30 mins of
    http://unity3d.com/learn/tutorials/modules/beginner/live-training-archive/space-shooter-to-mobile?playlist=17147

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviour {
 	private Rigidbody _rigidbody;
 	private AudioSource _audio;
 	private float nextFire = 0.0f;
+	private Quaternion calibrationQuaternion;
 
 	// public vars can be set from the properties in Unity itself
 	public float speed, tilt;
@@ -32,11 +33,21 @@ public class PlayerController : MonoBehaviour {
 		// getters
 		_rigidbody = GetComponent<Rigidbody> ();
 		_audio = GetComponent<AudioSource> ();
+
+		// in "reality", we'd call this from some sort of options settings
+		// so we calibrate every time the player is ready to play, but not as late
+		// as actually during the scene! This implementation will calibrate
+		// to whatever orientation the device physically was in when starting the scene.
+		CalibrateAccelerometer ();
 	}
 
 	// firing a shot doesn't require physics, so let's not wait for
 	// FixedUpdate
 	void Update(){
+
+		// magically enough, "Fire1" is derived from either mouse clicks on a computer
+		// OR any touch events on a device! So when testing this on the phone touches
+		// actually made the ship fire, which is pretty darn neat!
 		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
 			// in the inspectors, rotation quaternions are simplified as Euler quaternions
@@ -48,10 +59,22 @@ public class PlayerController : MonoBehaviour {
 
 	// called just before each fixed physics step
 	void FixedUpdate (){
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
+
+		// these are for the keyboard input, not using them for mobile
+		//float moveHorizontal = Input.GetAxis ("Horizontal");
+		//float moveVertical = Input.GetAxis("Vertical");
 		// remember: x -> left/right, y -> up/down, z -> forward/back
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		//Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		// get input from the accelerometer
+		// Using this as is (default zero point) I'd need to hold the device parallel to the floor
+		// otherwise the ship falls to the bottom of the screen. Need to calibrate the starting point so it tilts
+		// from that instead of actuall acceleration zero.
+		Vector3 accelerationRaw = Input.acceleration;
+		Vector3 acceleration = FixAcceleration (accelerationRaw);
+		// using you in the Z plane because that's how the orientation on devices
+		// would work. 
+		Vector3 movement = new Vector3 (acceleration.x, 0.0f, acceleration.y);
 
 		// by default the movement has a speed of 1
 		_rigidbody.velocity = movement * speed;
@@ -67,5 +90,18 @@ public class PlayerController : MonoBehaviour {
 
 		// set the tilt of the ship every time it moves
 		_rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, _rigidbody.velocity.x * -tilt);
+	}
+
+	/* Calibrate the input's raw acceleration by rotating to a plane that allows the player to hold it more naturally, vs parallel to the floor*/
+	void CalibrateAccelerometer(){
+		Vector3 accelerationSnapshot = Input.acceleration;
+		Quaternion rotateQuaternion = Quaternion.FromToRotation (new Vector3 (0.0f, 0.0f, -1.0f), accelerationSnapshot);
+		calibrationQuaternion = Quaternion.Inverse (rotateQuaternion);
+	}
+
+	/* Get the 'calibrated' value from the device input */
+	Vector3 FixAcceleration(Vector3 acceleration){
+		Vector3 fixedAcceleration = calibrationQuaternion * acceleration;
+		return fixedAcceleration;
 	}
 }
